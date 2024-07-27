cb2d245d18c2a76d98464362ebcd072bd48279c4
drisyx <drisyashenoyd@gmail.com>
2024-07-27 20:26:33 +0530

    project created

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AppHeader = () => {
     return (
@@ -7,34 +8,30 @@ const AppHeader = () => {
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
 
-                    <a class="navbar-brand" href="/">
+                    <Link class="navbar-brand" to="/">
                         <img src="https://www.shutterstock.com/image-vector/newspaper-vector-illustrationisolated-on-white-260nw-2076848707.jpg" alt="Bootstrap" width="30" height="24" />
-                    </a>
+                    </Link>
 
 
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Add</a>
+                                <Link class="nav-link active" aria-current="page" to="/">Add</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/delete">Delete</a>
+                                <Link class="nav-link" to="/delete">Delete</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/search">Search</a>
+                                <Link class="nav-link" to="/search">Search</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/view">ViewAll</a>
+                                <Link class="nav-link" to="/view">ViewAll</Link>
                             </li>
 
                         </ul>
                     </div>
                 </div>
             </nav>
-
-
-
-
 
         </div>
     )
